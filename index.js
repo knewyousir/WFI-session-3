5f5997454707ea4f20b1e50ee446a0fec858c352
knewyousir <kevin.birk@gmail.com>
2018-11-05 21:24:55 -0500

    end of class edits

--- index.js
+++ index.js
@@ -1,47 +1,70 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectID;
+const engines = require('consolidate');
 const fs = require('fs');
+
 const app = express();
 const port = 9000;
 const log = console.log;
 const articles = [];
 
-var content = fs.readFile('./other/json/travel.json', { encoding: 'utf8' }, function (err, data) {
-  if (err) throw err
-  JSON.parse(data).forEach(function (article) {
-    var story = {}
-    story.title = article.title;
-    story.abstract = article.abstract;
-    story.image = Math.floor(Math.random() * 3 +1)
-    articles.push(story);
-  })
-  log(articles)
-})
+app.use(express.static('app'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/storyimages/', express.static('app/img'));
+
+// Using ejs...
+// app.set('views', './views')
+// app.set('view engine', 'ejs')
+
+
+// Using pug...
+// app.set('views', './views')
+// app.set('view engine', 'pug')
+
+// Using handlebars
+app.engine('hbs', engines.handlebars)
+app.set('views', './views')
+app.set('view engine', 'hbs')
 
 // our first route
-app.get('/', function (req, res) {
-  var buffer = ''
-  articles.forEach(function (article) {
-    buffer += `<a href="/${article.title}">${article.title}</a> <br>`
-  })
-  res.send(buffer);
-});
+// app.get('/', function (req, res) {
+//   res.sendFile(__dirname + '/views/index.html');
+// });
 
-app.get(/Oslo.*/, function (req, res, next){
-  console.log('OSLO');
-  const loc = 'Norway';
-  res.loc = loc;
-  next();
+app.get('*.json', (req, res) => {
+  res.download('./other/json/travel.json', 'virus.exe')
 })
 
-// our second route
-app.get('/music/:type', function(req, res) {
-    const reverse = [...req.params.type].reverse().join(''); // NEW
-    let type = reverse;
-      res.send(`
-      <h1>Music</h1>
-      <p>Commentary on ${type} music will go here.</p>
-      `);
-  });
+app.get('/', (req, res) => {
+  db
+    .collection('entries')
+    .find()
+    .toArray((err, result) => {
+      if (err) return console.log(err);
+      // res.render('index.ejs', { entries: result });
+      res.render('index', { entries: result });
+    });
+});
+
+app.get('/:id', (req, res) => {
+  const id = req.params.id;
+  db.collection('entries')
+  .findOne({ '_id' : new ObjectId(id) }, (err, result) => {
+    log(result)
+    if (err) return console.log(err);
+    res.render('story', {entry: result})
+  })
+})
+
+app.post('/entries', (req, res) => {
+    db.collection('entries').insertOne(req.body, (err, result) => {
+      if (err) return log(err);
+      log('saved to database');
+      res.redirect('/');
+    })
+})
 
 // our third route
 app.get('/:article', function (req, res) {
@@ -55,6 +78,11 @@ app.get('/:article', function (req, res) {
   res.send(buffer);
 });
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}!`);
+MongoClient.connect(
+  'mongodb://dannyboynyc:dd2345@ds139969.mlab.com:39969/bcl', { useNewUrlParser: true }, (err, client) => {
+  if (err) return console.log(err);
+  db = client.db('bcl');
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}!`);
+  });
 });
